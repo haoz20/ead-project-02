99d1b5df0fb942e03537b9fbfa1894062f7e82ee
haoz20 <zhaonianhao2005@gmail.com>
2026-02-01 21:58:02 +0700

    Remove aws credentials in s3 configuration

--- backend/src/s3.js
+++ backend/src/s3.js
@@ -8,10 +8,6 @@ const path = require('path')
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 })
 
 const upload = multer({

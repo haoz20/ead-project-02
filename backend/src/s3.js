ed88999027c2ddefe5aae39c658cacbbdcb3656a
haoz20 <zhaonianhao2005@gmail.com>
2026-02-01 23:07:49 +0700

    add session token in s3

--- backend/src/s3.js
+++ backend/src/s3.js
@@ -11,6 +11,7 @@ const s3Client = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
   },
 })
 
